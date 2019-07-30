7983d571dd85c95af1fd01b211dc4163c4914c5c
Gonzalo <gonzalolascar@gmail.com>
2019-07-30 21:19:44 +0100

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+
+        let salida = {
+            name: 'Gonzalo',
+            age: 28,
+            url: request.url
+        }
+
+        response.write(JSON.stringify(salida));
+
+        response.end();
+
+    })
+    .listen(8080);
+
+
+
+console.log('Escuchando el puerto 8080...');
